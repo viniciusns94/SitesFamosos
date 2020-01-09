79d63ff84230ebc10894bc87713685a560d490b3
Vinicius Nogueira <vinicius.nogueira@unidesk.com.br>
2020-01-09 11:03:36 -0300

    Add project files.

---
+++ SitesFamosos/SitesFamosos/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace SitesFamosos
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void TapGestureRecognizer_Tapped1(object sender, EventArgs e)
+        {
+            Launcher.OpenAsync(new Uri("https://www.meutimao.com.br/"));
+        }
+
+        private void TapGestureRecognizer_Tapped2(object sender, EventArgs e)
+        {
+            Launcher.OpenAsync(new Uri("https://globoesporte.globo.com/"));
+        }
+
+        private void TapGestureRecognizer_Tapped3(object sender, EventArgs e)
+        {
+            Launcher.OpenAsync(new Uri("https://www.marvel.com/"));
+        }
+    }
+}
